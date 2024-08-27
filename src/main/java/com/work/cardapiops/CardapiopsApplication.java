7acd78579ec2b506531fb017244a7418d0f70259
Isaias Santos <ifelipes@gmail.com>
2024-08-26 22:09:24 -0300

    Adicionando arquivos do backend

---
+++ src/main/java/com/work/cardapiops/CardapiopsApplication.java
@@ -0,0 +1,13 @@
+package com.work.cardapiops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapiopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapiopsApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/work/cardapiops/food/FoodRequestDTO.java
@@ -0,0 +1,5 @@
+package com.work.cardapiops.food;
+
+public record FoodRequestDTO(String title, String image, Integer price ) {
+
+}
